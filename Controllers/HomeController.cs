6cc0f5bd8d7c91acce0397cf0c1813c25402e9ca
andrey <lo-st@ya.ru>
2020-09-28 18:30:37 +0400

    Display all coffee and price of current pricelist on webpage

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,48 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CoffeeBreak.Data;
+using CoffeeBreak.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CoffeeBreak.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext context;
+        public HomeController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        
         /// <summary>
         /// If user is authenticated display priceList for coffee and tea 
         /// If user not authenicated redirect to account/login
         /// </summary>
         /// <returns>View</returns>
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            ViewBag.Name = User.Identity.Name;
-            ViewBag.IsAuthenticated = User.Identity.IsAuthenticated;
-            return View();
+            var pricelist = await context.Pricelists.FirstOrDefaultAsync(p => p.NumberOfWeek == 39);
+            var coffeeProducts = await context.CoffeeProducts.Where(p => p.PricelistId == pricelist.Id).ToListAsync();
+
+            var models = new List<CoffeeViewModel>();
+            foreach(var coffee in coffeeProducts)
+            {
+                var model = new CoffeeViewModel
+                {
+                    Id = coffee.Id,
+                    Name = coffee.Name,
+                    PriceBigPacket = coffee.PriceBigPacket,
+                    PriceSmallPacket = coffee.PriceSmallPacket,
+                };
+
+                models.Add(model);
+            }
+
+            return View(models);
         }
 
         public IActionResult AccessDenied()
---
+++ Models/CoffeeViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeBreak.Models
+{
+    public enum BagSize
+    {
+        [Display(Name = "250г")]
+        SmallPacket,
+        [Display(Name = "1кг")]
+        BigPacket,
+    }
+
+    public enum Grind
+    {
+        [Display(Name = "Мелкий помол")]
+        FineGround = 1,
+        [Display(Name = "Грубый помол")]
+        CoarseGround,
+        [Display(Name = "Зерно")]
+        WholeBeans,
+    }
+
+    public class CoffeeViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+        
+        public decimal PriceBigPacket { get; set; }
+        
+        public decimal PriceSmallPacket { get; set; }
+
+        public Grind Ground { get; set; }
+
+        public BagSize Packet { get; set; }
+
+        public int Count { get; set; }
+    }
+}
